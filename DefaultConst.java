97c717e9e45f0b7a74c2d8f9ef43954a7e10f65c
SanketShewale <103431849+SanketShewale@users.noreply.github.com>
2022-07-26 19:11:18 +0530

    Add files via upload

---
+++ DefaultConst.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class DefaultConst {
+DefaultConst(){
+	System.out.println("Hellow World");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new DefaultConst();
+	}
+
+}
