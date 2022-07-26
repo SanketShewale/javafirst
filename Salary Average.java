2b6f11bc564d391849b9b3337944d73f4a4d9c06
SanketShewale <103431849+SanketShewale@users.noreply.github.com>
2022-07-26 18:58:44 +0530

    Add files via upload

---
+++ Salary Average.java
@@ -0,0 +1,14 @@
+public class SalaryAverage
+{
+
+public static void main(String[] args)
+   {
+ int average= 10000;
+ int x= 11000;
+ int y= 5000;
+ int z= 3*average-(x+y);
+
+System.out.println("The salary of third employee z is " +z);
+ }
+
+}
